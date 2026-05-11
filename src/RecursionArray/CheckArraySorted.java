97a44f8c3421b550f0434151aae98886c66aed05
Manas Sharma <officalmanassharma@gmail.com>
2026-05-11 17:40:35 +0530

    Check the arr is sorted in ascending order

---
+++ src/RecursionArray/CheckArraySorted.java
@@ -0,0 +1,13 @@
+package RecursionArray;
+
+public class CheckArraySorted {
+    static boolean helper (int [] arr , int idx){
+        if (idx == arr.length-1) return true;
+        return arr[idx] < arr[idx+1] && helper(arr,idx+1);
+    }
+
+    static void main(String[] args) {
+        int [] arr = {1,2,3,4,70,10,13};
+        System.out.println(helper(arr,0));
+    }
+}
