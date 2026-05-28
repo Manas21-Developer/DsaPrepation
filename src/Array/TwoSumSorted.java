ff6c36af77be5f2198b84b44983a72f471579cef
Manas Sharma <officalmanassharma@gmail.com>
2026-05-28 17:57:32 +0530

    Two sorted updated are easily solvew with the help of two pointer

---
+++ src/Array/TwoSumSorted.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class TwoSumSorted {
+    public static int[] twoSum(int[] numbers, int target) {
+        int s = 0 ;
+        int e = numbers.length-1;
+        while (s<e){
+            int sum = numbers[s]+numbers[e];
+            if (sum == target) return new int[] {s+1,e+1};
+            else if (sum >target) e--;
+            else s++;
+        }return new int [] {-1,-1};
+    }
+
+    static void main(String[] args) {
+        int[] arr = {2,5,6,78,9};
+        int [] ans =  twoSum(arr,8);
+        System.out.print("Target present at index :- " +ans[0] +","+ ans[1]);
+    }
+}
