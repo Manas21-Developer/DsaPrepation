d39e666122d70485be5718a24b734788157871ab
Manas Sharma <officalmanassharma@gmail.com>
2026-06-03 17:11:04 +0530

    Three summ optimize way

---
+++ src/Array/FindMaximusSumTriplet.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class FindMaximusSumTriplet {
+    static int helper (int [] arr,int n){
+        int ans = 0 ;
+        for (int i  = 0 ; i<n;i++){
+            for (int j= i+1;j<n-1-i;j++){
+                for (int k= j+1;k<n-2-j;j++){
+                    if (arr[i]<arr[j] && arr[j]<arr[k] &&
+                        ans < arr[i]+arr[j]+arr[k]){
+                        ans = arr[i]+arr[j]+arr[k];
+                    }
+                }
+            }
+        }return ans;
+    }
+
+    static void main(String[] args) {
+        int [] arr = {2 ,5 ,3 ,1 ,4 ,9};
+        System.out.print(helper(arr,6));
+    }
+}
