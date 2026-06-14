de0a5f1e148cb5a2390616cbe0b6cf165ff2ae79
Manas Sharma <officalmanassharma@gmail.com>
2026-06-14 20:47:36 +0530

    Que :- 21 | Merge Two sorted | Iterative Merge with dummy | TC : O(n+m) SC : O(1)

---
+++ src/LinkedList/MergeTwoSortedList.java
@@ -0,0 +1,64 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MergeTwoSortedList {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // List1: 1 -> 2 -> 4
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        // List2: 1 -> 3 -> 4
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode mergedHead = mergeTwoLists(list1, list2);
+
+        System.out.print("Merged List: ");
+        printList(mergedHead);
+    }
+}
