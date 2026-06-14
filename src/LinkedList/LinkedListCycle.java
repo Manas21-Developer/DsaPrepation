3591daec607a9a00a3fb94bc0d762bf8168b2161
Manas Sharma <officalmanassharma@gmail.com>
2026-06-14 21:02:01 +0530

    Que - 141 | Linked List cycle detection using Floyd's Tortoise and Hare algo |TC : O(n) SC : O(1)

---
+++ src/LinkedList/LinkedListCycle.java
@@ -0,0 +1,47 @@
+package LinkedList;
+
+public class LinkedListCycle {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+
+            // Creating nodes
+            ListNode head = new ListNode(3);
+            ListNode second = new ListNode(2);
+            ListNode third = new ListNode(0);
+            ListNode fourth = new ListNode(-4);
+
+            // Connecting nodes
+            head.next = second;
+            second.next = third;
+            third.next = fourth;
+
+            // Creating a cycle
+            fourth.next = second;
+
+            System.out.println("Cycle Present: " + hasCycle(head));
+    }
+}
