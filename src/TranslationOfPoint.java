862eab61da88e452d009cddeb96ff5dcdda3fda2
Manas Sharma <officalmanassharma@gmail.com>
2026-06-24 17:17:00 +0530

    Time pass question

---
+++ src/TranslationOfPoint.java
@@ -0,0 +1,20 @@
+public class TranslationOfPoint {
+        int x; int y;
+
+        public TranslationOfPoint(int x ,int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public void translate(int dx , int dy){
+            this.x += dx;
+            this.y += dy;
+        }
+    public static void main(String[] args) {
+        TranslationOfPoint obj = new TranslationOfPoint(2,3);
+        obj.translate(4,6);
+        System.out.println(obj.x +" "+obj.y);
+    }
+}
+
+
