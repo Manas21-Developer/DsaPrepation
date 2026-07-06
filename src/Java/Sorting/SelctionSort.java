3efc48eee5e0a800d61810c0e9133a797dcadd7f
Manas Sharma <officalmanassharma@gmail.com>
2026-07-06 23:02:30 +0530

    Ques :- GFG  | Using normal max logic and replace with the last idx and and after iteration change make a wall which help to segregate the sorted part and un sorted part | Tc O(n^2) Sc O(1)

--- src/Java/Sorting/SelctionSort.java
+++ src/Java/Sorting/SelctionSort.java
@@ -1,4 +1,27 @@
 package Java.Sorting;
 
 public class SelctionSort {
+    void selectionSort(int[] arr) {
+        for (int i = 0 ; i<arr.length;i++){
+            int lastIdx = arr.length-1 -i;
+            int maxIdx = helper(arr,lastIdx+1);
+            swap (arr,lastIdx,maxIdx);
+        }
+    }
+    public int helper(int [] arr , int len){
+        int max = Integer.MIN_VALUE;
+        int idx = -1;
+        for (int i = 0 ; i<len;i++){
+            if (arr[i] >max){
+                max = arr[i];
+                idx = i;
+            }
+        }return idx;
+    }
+
+    public void swap (int [] arr , int i ,int j){
+        int temp = arr[i];
+        arr[i] =arr[j];
+        arr[j]= temp;
+    }
 }
