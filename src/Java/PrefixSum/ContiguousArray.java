e40518eb7187eff2bbadf89c9b3da1059286ad07
Manas Sharma <officalmanassharma@gmail.com>
2026-07-08 23:22:07 +0530

    Ques :- 525 | with the help of Hash map and Prefix sum if the sum repeate again that we calculate the len | Tc O(n) Sc O(1)

---
+++ src/Java/PrefixSum/ContiguousArray.java
@@ -0,0 +1,23 @@
+package Java.PrefixSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContiguousArray {
+    public int findMaxLength(int[] nums) {
+        Map<Integer , Integer > map = new HashMap<>();
+        map.put(0,-1);
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for(int i = 0 ; i<nums.length;i++){
+            if(nums[i] == 0) sum--;
+            else sum++;
+
+            if (map.containsKey(sum)){
+                maxLen  = Math.max(maxLen , i-map.get(sum));
+            }else map.put(sum,i);
+        }return maxLen;
+    }
+}
