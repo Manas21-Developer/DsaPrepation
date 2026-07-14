475fd79f3d46b6c12ff6b5801113d68bca673399
Manas Sharma <officalmanassharma@gmail.com>
2026-07-14 21:21:58 +0530

    Ques 1021 | Stack + StringBuilder approach | TC O(N) SC O(N)

--- src/Java/String/RemoveOutermostParentheses.java
+++ src/Java/String/RemoveOutermostParentheses.java
@@ -1,4 +1,25 @@
 package Java.String;
 
 public class RemoveOutermostParentheses {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0 ; i<s.length();i++){
+            char ch = s.charAt(i);
+            if (ch == '('){
+                if (st.isEmpty()){
+                    st.push(ch);
+                }else {
+                    st.push(ch);
+                    ans.append(ch);
+                }
+            }else {
+                st.pop();
+                if(!st.isEmpty()){
+                    ans.append(ch);
+                }
+            }
+        }return ans.toString();
+    }
 }
