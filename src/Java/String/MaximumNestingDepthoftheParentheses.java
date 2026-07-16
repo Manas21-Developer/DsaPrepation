228ef582ed1125f29f6b295e6cec63c58ca8930f
Manas Sharma <officalmanassharma@gmail.com>
2026-07-16 21:31:59 +0530

    Ques :- 1614 | Track current nesting depth using counter and stack | Update maximum depth while traversing | TC O(n) SC O(n)

--- src/Java/String/MaximumNestingDepthoftheParentheses.java
+++ src/Java/String/MaximumNestingDepthoftheParentheses.java
@@ -1,4 +1,27 @@
 package Java.String;
 
+import java.util.Stack;
+
 public class MaximumNestingDepthoftheParentheses {
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        Stack<Character> st = new Stack<>();
+        int cnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                cnt++;
+                st.push(ch);
+                maxDepth = Math.max(maxDepth, cnt);
+            }
+            else if (ch == ')') {
+                cnt--;
+                st.pop();
+            }
+        }
+        return maxDepth;
+    }
+
 }
