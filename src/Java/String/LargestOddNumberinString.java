b792b2694d34bfac1c9b6cbc64ac38b8e0bc9b9b
Manas Sharma <officalmanassharma@gmail.com>
2026-07-16 21:36:00 +0530

    Ques :- 1903 | Find rightmost odd digit by traversing from end | Return prefix using substring() | TC O(n) SC O(1)

---
+++ src/Java/String/LargestOddNumberinString.java
@@ -0,0 +1,17 @@
+package Java.String;
+
+public class LargestOddNumberinString {
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            char digit = num.charAt(i);
+
+            if (digit == '1' || digit == '3' ||
+                    digit == '5' || digit == '7' ||
+                    digit == '9') {
+
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
